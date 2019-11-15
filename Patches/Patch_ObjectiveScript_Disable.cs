9a46603a6ff6806f9a01f24419859b6ae14e5032
SuperKael <kaelhaney@gmail.com>
2019-11-14 20:07:40 -0500

    Fixed items dropped from the object:objective loot table being impossible to pick up.

--- Patches/Patch_ObjectiveScript_Disable.cs
+++ Patches/Patch_ObjectiveScript_Disable.cs
@@ -13,7 +13,7 @@ namespace GadgetCore.Patches
         [HarmonyPostfix]
         public static void Postfix(ObjectiveScript __instance)
         {
-            LootTables.DropLoot("object:objective", __instance.transform.position, 0.1f);
+            LootTables.DropLoot("object:objective", new Vector3(__instance.transform.position.x, __instance.transform.position.y, 0f), 0.1f);
         }
     }
 }
