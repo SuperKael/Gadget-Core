be3d86750fcec0c2cdca469f9af217ab5493abc5
SuperKael <kaelhaney@gmail.com>
2019-12-25 15:38:04 -0500

    Add AssetBundleCreator

---
+++ Editor/AssetBundleCreator.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+
+public class AssetBundleCreator {
+    [MenuItem("Assets/Build Asset Bundles")]
+	public static void BuildBundles()
+    {
+		string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
+#endif
