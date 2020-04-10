d8547261a51d6fdb68eec9163c01fb92d22a9cf5
SuperKael <kaelhaney@gmail.com>
2020-04-10 12:19:32 -0500

    Some improvements to the template mod.

--- TemplateGadgetMod/Properties/AssemblyInfo.cs
+++ TemplateGadgetMod/Properties/AssemblyInfo.cs
@@ -3,10 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Template Gadget Mod")] //Set this to the full name of the mod including spaces.
 [assembly: AssemblyTitle("Template Gadget Mod")] //This is only used when mousing over a dll file in Windows explorer.
-[assembly: AssemblyDescription("A mod for Roguelands' GadgetCore")] //This is a short description for your mod's assembly.
+[assembly: AssemblyDescription("A Gadget mod for Roguelands")] //This is a short description for your mod's assembly.
 [assembly: AssemblyCompany("")] //Set this to your name/nickname and/or website
 [assembly: AssemblyCopyright("© 20XX TemplateDeveloper. All rights reserved.")] //Set this to your copyright name.
 [assembly: AssemblyVersion(TemplateGadgetMod.MOD_VERSION)]
 [assembly: AssemblyFileVersion(TemplateGadgetMod.MOD_VERSION)]
-
-[assembly: ComVisible(false)]
